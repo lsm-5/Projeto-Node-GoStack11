e57c1f4514b1849211891f1a49b78a93f4e2c89f
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-04-13 11:32:26 -0300

    config: eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello Programmer" });
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello Programmer' });
 });
 
 app.listen(3333, () => {
-  console.log("🖥️ Server started on port 3333");
+  console.log('🖥️ Server started on port 3333');
 });
